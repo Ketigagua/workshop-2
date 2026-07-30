96a7fe8d7af83b9be9115bab9f22b0297766ea91
BTUmentoring <mentorship-front@btu.edu.ge>
2026-07-30 15:15:08 +0400

    workshop3

---
+++ app.js
@@ -0,0 +1,28 @@
+//დავალება 1
+function checkEvenOrOdd(number) {
+  // TODO: დაწერე if/else კონსტრუქცია
+  // თუ number ლუწია, დაბეჭდე "even"
+  // თუ number კენტია, დაბეჭდე "odd"
+}
+
+// checkEvenOrOdd(2);
+// checkEvenOrOdd(4);
+
+//დავალება 2
+
+//დავალება 3
+
+function pizzaPrice() {
+  //გაიგე აქ ფრომფთებით პიცის ზომა და შეინაზე ცვლადში size
+  //გაიგე დამატებით ყველაი უნდა თუ არა, ჩაწეროს yes/no
+
+  let price;
+
+  // TODO: switch — დაადგინე price ზომის მიხედვით
+
+  // TODO: if/else — დაამატე 3, თუ extraCheese === "yes"
+
+  console.log(`შენი პიცის ფასია: ${price} ლარი`);
+}
+
+//pizzaPrice();
